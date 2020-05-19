2e602c45569ed6d757ac3fbb1f8c7b1a335e73f0
taoli <15230817603@163.com>
2020-05-19 22:23:18 +0800

    login

--- my-react-web/src/pages/Login.js
+++ my-react-web/src/pages/Login.js
@@ -1,13 +1,69 @@
 import React from 'react'
 import '../style/login/login.less'
+
+import { Input, Button, message} from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
 export default class Login extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            userPassword: ''
+        }
+    }
+
     render(){
         return(
-            <div>
-                <div className='demo'>
-                Login页面
+            <div className='login'>
+                <div className='login-content'>
+                    <Input size="large" placeholder="请输入用户名" prefix={<UserOutlined />} style={{ width: '300px', marginBottom: '10px' }} onChange={event => this.getUserName(event)} />
+                    <Input.Password size='large' placeholder="请输入密码" prefix={<LockOutlined />} style={{ width: '300px', marginBottom: '10px' }} onChange={event => this.getUserPassword(event)} />
+                    <span className='userTip'>用户名：admin，密码：admin</span>
+                    <Button type="primary" style={{ width: '300px' }} onClick={this.login}>登录</Button>
                 </div>
             </div>
         )
     }
+
+    // 用户名
+    getUserName(event) {
+        if (event && event.target && event.target.value) {
+            let userName = event.target.value;
+            this.setState(() => ({ userName: userName }))
+        }
+    }
+    // 密码
+    getUserPassword(event) {
+        if (event && event.target && event.target.value) {
+            let userPassword = event.target.value;
+            this.setState(() => ({ userPassword: userPassword }))
+        }
+    }
+
+    //登录
+    login = () => {
+        
+        console.log('点击登录', this.state.userName, this.state.userPassword)
+        if(this.state.userName != 'admin'){
+            message.error('用户名错误！');
+            return
+        }
+        if(this.state.userPassword != 'admin'){
+            message.error('密码错误！');
+            return
+        }
+
+        console.log('dengl')
+
+        // localStorage.setItem('userInfo',true)
+        // console.log('跳转至详情页面，同时将登录状态保存进localstorage',this)
+        // this.props.history.push('/content')
+
+        
+        // dispatch(updateUserName(this.state.userName))
+        
+    }
+
+
 }
