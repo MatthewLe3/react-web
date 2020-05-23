4857fe0ade89e0981ea464596c6a0964a0e4a217
taoli <15230817603@163.com>
2020-05-23 21:49:47 +0800

    antv

--- my-react-web/src/components/Home.js
+++ my-react-web/src/components/Home.js
@@ -4,6 +4,8 @@ import '../style/content/home.less'
 import { Tooltip, Progress } from 'antd'
 import { ExclamationCircleOutlined, CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons';
 
+import { Chart } from '@antv/g2';
+
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -14,6 +16,22 @@ export default class Home extends React.Component {
                 { title: '访问量', num: '8,846', type: 'line', data: ['12%', '11%'], footerData: { text: '日访问量', num: '1,234' } },
                 { title: '支付比数', num: '6,560', type: 'bar', data: ['12%', '11%'], footerData: { text: '转化率', num: '60%' } },
                 { title: '运营活动效果', num: '78%', type: 'progress', data: [78], footerData: { text: ['周同比', '日同比'], num: ['12%', '11%'] } },
+            ],
+            payData: [
+                { date: '2020-05-01', num: 275 },
+                { date: '2020-05-02', num: 115 },
+                { date: '2020-05-03', num: 120 },
+                { date: '2020-05-04', num: 350 },
+                { date: '2020-05-05', num: 150 },
+                { date: '2020-05-06', num: 321 },
+                { date: '2020-05-07', num: 132 },
+                { date: '2020-05-08', num: 156 },
+                { date: '2020-05-09', num: 264 },
+                { date: '2020-05-10', num: 136 },
+                { date: '2020-05-11', num: 78 },
+                { date: '2020-05-12', num: 123 },
+                { date: '2020-05-13', num: 311 },
+                { date: '2020-05-14', num: 201 },
             ]
         }
     }
@@ -52,6 +70,13 @@ export default class Home extends React.Component {
         )
     }
 
+    componentDidMount() {
+        setTimeout(() => {
+            this.drawChart()
+        }, 100);
+        
+    }
+
     bodyHtml = val => {
 
         if (val.type == 'compare') {
@@ -68,11 +93,11 @@ export default class Home extends React.Component {
             )
         } else if (val.type == 'line') {
             return (
-                <div className='chart-content'>aa</div>
+                <div id='visitedChart' className='chart-content'></div>
             )
         } else if (val.type == 'bar') {
             return (
-                <div className='chart-content'>bb</div>
+                <div  id='payChart' className='chart-content'></div>
             )
         } else if (val.type == 'progress') {
             return (
@@ -102,5 +127,43 @@ export default class Home extends React.Component {
                 </div>
             )
         }
+    }
+
+    drawChart = () => {
+        const data = this.state.payData
+
+        // Step 1: 创建 Chart 对象
+        const chart = new Chart({
+            container: 'payChart', // 指定图表容器 ID
+            // width: document.querySelector('#payChart').offsetWidth, // 指定图表宽度
+            height: document.querySelector('#payChart').offsetHeight, // 指定图表高度
+            padding: [20, 10, 0, 10],
+            autoFit: true,
+        });
+
+        // Step 2: 载入数据源
+        chart.data(data);
+        
+
+        // Step 3: 创建图形语法，绘制柱状图
+        chart.axis('num', false)
+        .interval()
+        .position('date*num')
+        // .label('num',{
+        //     offset: 10, // 文本距离图形的距离
+        //     textStyle: {
+        //       fill: '#404040',
+        //       fontSize:'122',
+        //       fontWeight: 'bold', // 文本粗细
+        //       shadowBlur: 5, // 文本阴影模糊
+        //       shadowColor: '#fff' // 阴影颜色
+        //     },
+        //     formatter:function (value) {
+        //       return value
+        //     }
+        // });
+
+        // Step 4: 渲染图表
+        chart.render();
     }
 }
