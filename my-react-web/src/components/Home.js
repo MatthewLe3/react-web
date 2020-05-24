df37d9192ade060c819c60c5613862cb809a9f3c
taoli <15230817603@163.com>
2020-05-24 19:57:13 +0800

    homebody

--- my-react-web/src/components/Home.js
+++ my-react-web/src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../style/content/home.less'
 
 import { Tooltip, Progress, Tabs, DatePicker } from 'antd'
+import { Row, Col } from 'antd';
 import { ExclamationCircleOutlined, CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons';
 import moment from 'moment';
 
@@ -61,7 +62,30 @@ export default class Home extends React.Component {
             ],
             startDate: '2020-01-01',
             endDate: '2020-01-02',
-            dateType:'today'
+            dateType: 'today',
+            saleDate: [
+                { date: '1月', num: 275 },
+                { date: '2月', num: 432 },
+                { date: '3月', num: 253 },
+                { date: '4月', num: 1235 },
+                { date: '5月', num: 753 },
+                { date: '6月', num: 324 },
+                { date: '7月', num: 263 },
+                { date: '8月', num: 563 },
+                { date: '9月', num: 931 },
+                { date: '10月', num: 293 },
+                { date: '11月', num: 126 },
+                { date: '12月', num: 742 },
+            ],
+            rankList: [
+                { name: '工专路0号店', num: '321,435' },
+                { name: '工专路1号店', num: '321,435' },
+                { name: '工专路2号店', num: '321,435' },
+                { name: '工专路3号店', num: '321,435' },
+                { name: '工专路4号店', num: '321,435' },
+                { name: '工专路5号店', num: '321,435' },
+                { name: '工专路6号店', num: '321,435' },
+            ]
         }
     }
 
@@ -72,9 +96,9 @@ export default class Home extends React.Component {
             <div className={this.state.dateType == 'week' ? 'changeDate activeDate' : 'changeDate'} onClick={() => { this.getDate('week') }}>本周</div>
             <div className={this.state.dateType == 'month' ? 'changeDate activeDate' : 'changeDate'} onClick={() => { this.getDate('month') }}>本月</div>
             <div className={this.state.dateType == 'year' ? 'changeDate activeDate' : 'changeDate'} onClick={() => { this.getDate('year') }}>本年</div>
-            <RangePicker value={[moment(this.state.startDate, dateFormat), moment(this.state.endDate, dateFormat)]} onChange={(date,dateString)=>this.changeDate(date,dateString)} />
+            <RangePicker value={[moment(this.state.startDate, dateFormat), moment(this.state.endDate, dateFormat)]} onChange={(date, dateString) => this.changeDate(date, dateString)} />
         </div>;
-        
+
 
         return (
             <div>
@@ -107,13 +131,13 @@ export default class Home extends React.Component {
                 </div>
                 <div className='body-content'>
                     <div>
-                        <Tabs defaultActiveKey="1" onChange={callback} className='body-tab' tabBarExtraContent={operations}>
+                        <Tabs defaultActiveKey="1" onChange={(key) => this.callback(key)} className='body-tab' tabBarExtraContent={operations}>
                             <TabPane tab="销售额" key="1">
-                                Content of Tab Pane 1
-    </TabPane>
+                                {this.dateHtml('销售趋势', '门店销售额排名')}
+                            </TabPane>
                             <TabPane tab="访问量" key="2">
-                                Content of Tab Pane 2
-    </TabPane>
+                                {this.dateHtml_('访问量趋势', '门店访问量排名')}
+                            </TabPane>
                         </Tabs>
 
                     </div>
@@ -248,50 +272,212 @@ export default class Home extends React.Component {
 
         visitedChart.axis('num', false)
         visitedChart.render();
+
+
+        // 销售量的图
+        const saleDate = this.state.saleDate
+        const saleChart = new Chart({
+            container: 'barChart', // 指定图表容器 ID
+            // width: document.querySelector('#payChart').offsetWidth, // 指定图表宽度
+            height: document.querySelector('#barChart').offsetHeight, // 指定图表高度
+            padding: [20, 30, 30, 30],
+            autoFit: true,
+        });
+        saleChart.data(saleDate);
+
+        saleChart
+            .interval()
+            .position('date*num')
+            .label('num', {
+                offset: 10, // 文本距离图形的距离
+                textStyle: {
+                    fill: '#404040',
+                    fontSize: '122',
+                    fontWeight: 'bold', // 文本粗细
+                    shadowBlur: 5, // 文本阴影模糊
+                    shadowColor: '#fff' // 阴影颜色
+                },
+                formatter: function (value) {
+                    return value
+                }
+            })
+            .color('', function () {
+                return '#64b0ff'
+            });
+
+        saleChart.tooltip({
+            // showMarkers: false, // 不展示 tooltip markers
+            showCrosshairs: true,
+        });
+        chart.interaction('active-region'); // 使用 active-region 交互行为
+
+        // saleChart.axis('num', false)
+        saleChart.render();
+
+
+
     }
 
-    getDate = (type)  => {
-        
-        if(type == 'today'){
-            this.setState({
-                startDate:dayjs().format('YYYY-MM-DD'),
-                endDate:dayjs().format('YYYY-MM-DD'),
-                dateType:'today'
+    drawChart2 = () => {
+        const shopVisitedDate = this.state.saleDate
+        const shopVisitedChart = new Chart({
+            container: 'barChart2', // 指定图表容器 ID
+            // width: document.querySelector('#payChart').offsetWidth, // 指定图表宽度
+            height: document.querySelector('#barChart2').offsetHeight, // 指定图表高度
+            padding: [20, 30, 30, 30],
+            autoFit: true,
+        });
+        shopVisitedChart.data(shopVisitedDate);
+
+        shopVisitedChart
+            .interval()
+            .position('date*num')
+            .label('num', {
+                offset: 10, // 文本距离图形的距离
+                textStyle: {
+                    fill: '#404040',
+                    fontSize: '122',
+                    fontWeight: 'bold', // 文本粗细
+                    shadowBlur: 5, // 文本阴影模糊
+                    shadowColor: '#fff' // 阴影颜色
+                },
+                formatter: function (value) {
+                    return value
+                }
             })
-        }else if(type == 'week'){
+            .color('', function () {
+                return '#64b0ff'
+            });
+
+        shopVisitedChart.tooltip({
+            // showMarkers: false, // 不展示 tooltip markers
+            showCrosshairs: true,
+        });
+        shopVisitedChart.interaction('active-region'); // 使用 active-region 交互行为
+
+        shopVisitedChart.render();
+    }
+
+    getDate = (type) => {
+
+        if (type == 'today') {
+            this.setState({
+                startDate: dayjs().format('YYYY-MM-DD'),
+                endDate: dayjs().format('YYYY-MM-DD'),
+                dateType: 'today'
+            })
+        } else if (type == 'week') {
             let dayLeft = 7 - dayjs().day()
 
-            if(dayjs().day() == 0) dayLeft = 0
-            
+            if (dayjs().day() == 0) dayLeft = 0
+
             this.setState({
-                startDate:dayjs().subtract(7-dayjs().day(), 'day').format('YYYY-MM-DD'),
-                endDate:dayjs().add(dayLeft, 'day').format('YYYY-MM-DD'),
-                dateType:'week'
+                startDate: dayjs().subtract(7 - dayjs().day(), 'day').format('YYYY-MM-DD'),
+                endDate: dayjs().add(dayLeft, 'day').format('YYYY-MM-DD'),
+                dateType: 'week'
             })
 
-        }else if(type == 'month'){
+        } else if (type == 'month') {
             this.setState({
-                startDate:dayjs().startOf('month').format('YYYY-MM-DD'),
-                endDate:dayjs().endOf('month').format('YYYY-MM-DD'),
-                dateType:'month'
+                startDate: dayjs().startOf('month').format('YYYY-MM-DD'),
+                endDate: dayjs().endOf('month').format('YYYY-MM-DD'),
+                dateType: 'month'
             })
-        }else{
+        } else {
             this.setState({
-                startDate:dayjs().startOf('year').format('YYYY-MM-DD'),
-                endDate:dayjs().endOf('year').format('YYYY-MM-DD'),
-                dateType:'year'
+                startDate: dayjs().startOf('year').format('YYYY-MM-DD'),
+                endDate: dayjs().endOf('year').format('YYYY-MM-DD'),
+                dateType: 'year'
             })
         }
     }
 
-    changeDate = (date,dateString) => {
+    changeDate = (date, dateString) => {
         this.setState({
-            startDate:dateString[0],
-            endDate:dateString[1]
+            startDate: dateString[0],
+            endDate: dateString[1]
         })
     }
+
+    dateHtml = (label, tableLabel) => {
+        console.log('aa', this.state.rankList)
+
+        let tableList = this.state.rankList.map((val, index) => {
+            return (
+                <li key={index} style={{ display: 'flex', lineHeight: '35px', height: '35px' }}>
+                    <div>
+                        <div className={index < 3 ? 'front' : 'unfront'}>
+                            <span>{index + 1}</span>
+                        </div>
+                    </div>
+                    <div className='tableName'>{val.name}</div>
+                    <div className='tableNum'>{val.num}</div>
+                </li>
+            )
+        })
+        return (
+            <div>
+                <Row>
+                    <Col span={18} push={0}>
+                        <h4 style={{ padding: '10px 20px 10px 0' }}>{label}</h4>
+                        <div id='barChart' style={{ height: '274px', padding: '10px 20px' }}></div>
+                    </Col>
+                    <Col span={6} pull={0}>
+                        <h4 style={{ padding: '10px 20px 10px 0' }}>{tableLabel}</h4>
+                        <div>
+                            <ul style={{ padding: '0' }}>
+                                {tableList}
+                            </ul>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+
+    dateHtml_ = (label, tableLabel) => {
+        console.log('aa', this.state.rankList)
+
+        let tableList = this.state.rankList.map((val, index) => {
+            return (
+                <li key={index} style={{ display: 'flex', lineHeight: '35px', height: '35px' }}>
+                    <div>
+                        <div className={index < 3 ? 'front' : 'unfront'}>
+                            <span>{index + 1}</span>
+                        </div>
+                    </div>
+                    <div className='tableName'>{val.name}</div>
+                    <div className='tableNum'>{val.num}</div>
+                </li>
+            )
+        })
+        return (
+            <div>
+                <Row>
+                    <Col span={18} push={0}>
+                        <h4 style={{ padding: '10px 20px 10px 0' }}>{label}</h4>
+                        <div id='barChart2' style={{ height: '274px', padding: '10px 20px' }}></div>
+                    </Col>
+                    <Col span={6} pull={0}>
+                        <h4 style={{ padding: '10px 20px 10px 0' }}>{tableLabel}</h4>
+                        <div>
+                            <ul style={{ padding: '0' }}>
+                                {tableList}
+                            </ul>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+
+    callback = key => {
+        if (key == 2) {
+            setTimeout(() => {
+                this.drawChart2()
+            }, 100);
+        }
+    }
+
 }
 
-function callback(key) {
-    console.log(key);
-}
