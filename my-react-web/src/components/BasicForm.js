c73c37ca12d00b63046c870c2320feab53dff6d9
taoli <15230817603@163.com>
2020-05-24 22:25:03 +0800

    basic-form

--- my-react-web/src/components/BasicForm.js
+++ my-react-web/src/components/BasicForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../style/content/basic-form.less'
 
 export default class BasicFrom extends React.Component{
     constructor(props){
@@ -7,8 +8,16 @@ export default class BasicFrom extends React.Component{
 
     render(){
         return (
-            <div>
-                BasicFrom
+            <div className='basic-form'>
+                <div className='basic-form-title'>
+                    <div>基础表单</div>
+                    <div className='basic-form-descript'>
+                        表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。
+                    </div>
+                </div>
+                <div className='basic-form-content'>
+                    content
+                </div>
             </div>
         )
     }
