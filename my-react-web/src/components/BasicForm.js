532a195c1cc7f178ffecbbb35ce4c0aeefe8b2bd
taoli <15230817603@163.com>
2020-05-25 22:29:17 +0800

    button-loading

--- my-react-web/src/components/BasicForm.js
+++ my-react-web/src/components/BasicForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../style/content/basic-form.less'
 
-import { Form, DatePicker, Button, Input, InputNumber, Radio, Tooltip } from 'antd';
+import { Form, DatePicker, Button, Input, InputNumber, Radio, Tooltip,message } from 'antd';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 
 const { RangePicker } = DatePicker;
@@ -91,6 +91,9 @@ const formConfig = {
 export default class BasicFrom extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            loading:false
+        }
     }
 
     render() {
@@ -103,96 +106,104 @@ export default class BasicFrom extends React.Component {
                     </div>
                 </div>
                 <div className='basic-form-content'>
-                    {TimeRelatedForm()}
+                    {this.TimeRelatedForm()}
                 </div>
             </div>
         )
     }
+
+    TimeRelatedForm = () => {
+        const onFinish = fieldsValue => {
+            this.setState({
+                loading:true
+            })
+            const rangeValue = fieldsValue['range-picker'];
+            const values = {
+                ...fieldsValue,
+                'title-value': fieldsValue['title-value'],
+                'range-picker': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
+                'describe': fieldsValue['describe'],
+                'standard': fieldsValue['standard'],
+                'user': fieldsValue['user'],
+                'invite': fieldsValue['invite'],
+                'weight': fieldsValue['weight'],
+                'is-public': fieldsValue['is-public'],
+            };
+            console.log('Received values of form: ', values);
+            setTimeout(() => {
+                this.setState({
+                    loading:false
+                })
+                message.success('表单数据提交成功！');
+            }, 3000);
+        };
+        return (
+            <Form name="time_related_controls" {...formItemLayout} onFinish={onFinish} initialValues={{
+                ['is-public']: 'public',
+            }}>
+                <Form.Item name="title-value" label="标题" {...formConfig['title-value']}>
+                    <Input placeholder={'请输入标题'} style={{ width: 450 }} />
+                </Form.Item>
+                <Form.Item name="range-picker" label="起始日期" {...rangeConfig}>
+                    <RangePicker placeholder={['开始日期', '结束日期']} style={{ width: 450 }} />
+                </Form.Item>
+                <Form.Item name="describe" label="目标描述" {...formConfig['describe']}>
+                    <TextArea rows={4} placeholder={'请输入你的阶段性工作目标'} style={{ width: 450 }} />
+                </Form.Item>
+                <Form.Item name="standard" label="衡量标准" {...formConfig['standard']}>
+                    <TextArea rows={4} placeholder={'请输入衡量标准'} style={{ width: 450 }} />
+                </Form.Item>
+                <Form.Item name="user" label={
+                    <span>
+                        客户<span style={{ marginLeft: '10px', color: '#949494', fontWeight: 200 }}>(选填)</span>
+                        <Tooltip title="目标的服务对象">
+                            <ExclamationCircleOutlined style={{ cursor: 'pointer', marginLeft: '10px', color: '#949494' }} />
+                        </Tooltip>
+                    </span>
+                } {...formConfig['user']}>
+                    <Input placeholder={'请描述里服务的客户，内部客户直接@姓名/工号'} style={{ width: 450 }} />
+                </Form.Item>
+                <Form.Item name="invite" label={
+                    <span>
+                        邀评人<span style={{ marginLeft: '10px', color: '#949494', fontWeight: 200 }}>(选填)</span>
+                    </span>
+                } {...formConfig['invite']}>
+                    <Input placeholder={'请直接@姓名/工号，最多可邀评5人'} style={{ width: 450 }} />
+                </Form.Item>
+                <Form.Item name="weight" label={
+                    <span>
+                        权重<span style={{ marginLeft: '10px', color: '#949494', fontWeight: 200 }}>(选填)</span>
+                    </span>
+                } {...formConfig['weight']}>
+                    <InputNumber />
+                </Form.Item>
+                <Form.Item name="is-public" label="目标公开" >
+                    <Radio.Group>
+                        <Radio value={"public"}>公开</Radio>
+                        <Radio value={"private"}>不公开</Radio>
+                    </Radio.Group>
+                </Form.Item>
+    
+    
+                <Form.Item
+                    wrapperCol={{
+                        xs: {
+                            span: 24,
+                            offset: 0,
+                        },
+                        sm: {
+                            span: 16,
+                            offset: 8,
+                        },
+                    }}
+                >
+                    <Button type="primary" htmlType="submit" loading={this.state.loading}>
+                        提交
+              </Button>
+                </Form.Item>
+            </Form>
+        );
+    };
 }
 
-let formLayout = 'public'
-const TimeRelatedForm = () => {
-    const onFinish = fieldsValue => {
-        // fieldsValue['is-public'] = 'public'
-        const rangeValue = fieldsValue['range-picker'];
-        const values = {
-            ...fieldsValue,
-            'title-value': fieldsValue['title-value'],
-            'range-picker': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
-            'describe': fieldsValue['describe'],
-            'standard': fieldsValue['standard'],
-            'user': fieldsValue['user'],
-            'invite': fieldsValue['invite'],
-            'weight': fieldsValue['weight'],
-            'is-public': fieldsValue['is-public'],
-        };
-        console.log('Received values of form: ', values);
-    };
-
-    return (
-        <Form name="time_related_controls" {...formItemLayout} onFinish={onFinish} initialValues={{
-            ['is-public']: 'public'
-        }}>
-            <Form.Item name="title-value" label="标题" {...formConfig['title-value']}>
-                <Input placeholder={'请输入标题'} style={{ width: 450 }} />
-            </Form.Item>
-            <Form.Item name="range-picker" label="起始日期" {...rangeConfig}>
-                <RangePicker placeholder={['开始日期', '结束日期']} style={{ width: 450 }} />
-            </Form.Item>
-            <Form.Item name="describe" label="目标描述" {...formConfig['describe']}>
-                <TextArea rows={4} placeholder={'请输入你的阶段性工作目标'} style={{ width: 450 }} />
-            </Form.Item>
-            <Form.Item name="standard" label="衡量标准" {...formConfig['standard']}>
-                <TextArea rows={4} placeholder={'请输入衡量标准'} style={{ width: 450 }} />
-            </Form.Item>
-            <Form.Item name="user" label={
-                <span>
-                    客户<span style={{ marginLeft: '10px', color: '#949494', fontWeight: 200 }}>(选填)</span>
-                    <Tooltip title="目标的服务对象">
-                        <ExclamationCircleOutlined style={{ cursor: 'pointer', marginLeft: '10px', color: '#949494' }} />
-                    </Tooltip>
-                </span>
-            } {...formConfig['user']}>
-                <Input placeholder={'请描述里服务的客户，内部客户直接@姓名/工号'} style={{ width: 450 }} />
-            </Form.Item>
-            <Form.Item name="invite" label={
-                <span>
-                    邀评人<span style={{ marginLeft: '10px', color: '#949494', fontWeight: 200 }}>(选填)</span>
-                </span>
-            } {...formConfig['invite']}>
-                <Input placeholder={'请直接@姓名/工号，最多可邀评5人'} style={{ width: 450 }} />
-            </Form.Item>
-            <Form.Item name="weight" label={
-                <span>
-                    权重<span style={{ marginLeft: '10px', color: '#949494', fontWeight: 200 }}>(选填)</span>
-                </span>
-            } {...formConfig['weight']}>
-                <InputNumber />
-            </Form.Item>
-            <Form.Item name="is-public" label="目标公开" >
-                <Radio.Group>
-                    <Radio value={"public"}>公开</Radio>
-                    <Radio value={"private"}>不公开</Radio>
-                </Radio.Group>
-            </Form.Item>
-
-
-            <Form.Item
-                wrapperCol={{
-                    xs: {
-                        span: 24,
-                        offset: 0,
-                    },
-                    sm: {
-                        span: 16,
-                        offset: 8,
-                    },
-                }}
-            >
-                <Button type="primary" htmlType="submit">
-                    提交
-          </Button>
-            </Form.Item>
-        </Form>
-    );
-};
+// const 
