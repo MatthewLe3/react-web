00031e5e7379a9fe36f76793d4c54368e15dba7c
taoli <15230817603@163.com>
2020-05-25 22:59:04 +0800

    form

--- my-react-web/src/components/AdvancedFrom.js
+++ my-react-web/src/components/AdvancedFrom.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../style/content/advanced-form.less'
 
 export default class AdvancedFrom extends React.Component{
     constructor(props){
@@ -7,8 +8,16 @@ export default class AdvancedFrom extends React.Component{
 
     render(){
         return (
-            <div>
-                AdvancedFrom
+            <div className='advanced-form'>
+                <div className='advanced-form-title'>
+                    <div>高级表单</div>
+                    <div className='advanced-form-descript'>
+                    高级表单常见于一次性输入和提交大批量数据的场景。
+                    </div>
+                </div>
+                <div className='advanced-form-content'>
+                    jj
+                </div>
             </div>
         )
     }
--- my-react-web/src/components/StepFrom.js
+++ my-react-web/src/components/StepFrom.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../style/content/step-form.less'
 
 export default class StepForm extends React.Component{
     constructor(props){
@@ -7,8 +8,16 @@ export default class StepForm extends React.Component{
 
     render(){
         return (
-            <div>
-                StepForm
+            <div className='step-form'>
+                <div className='step-form-title'>
+                    <div>分步表单</div>
+                    <div className='step-form-descript'>
+                    将一个冗长或用户不熟悉的表单任务分成多个步骤，指导用户完成。
+                    </div>
+                </div>
+                <div className='step-form-content'>
+                    ff
+                </div>
             </div>
         )
     }
