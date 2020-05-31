059b7bf8af94c9341bf68db992cc52b01ae86015
taoli <15230817603@163.com>
2020-05-31 20:52:07 +0800

    success,fail

--- my-react-web/src/components/Fail.js
+++ my-react-web/src/components/Fail.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import { CloseCircleFilled } from '@ant-design/icons';
 
-export default class Fail extends React.Component{
-    constructor(props){
+export default class Fail extends React.Component {
+    constructor(props) {
         super(props)
     }
 
-    render(){
+    render() {
         return (
-            <div>
-                fail
+            <div style={{ textAlign: 'center', padding: '24px', backgroundColor: '#fff' }}>
+                <CloseCircleFilled style={{ fontSize: '72px', color: '#ff4d4f' }} />
+                <div style={{
+                    fontSize: '24px', color: '#000000d9',
+                    lineHeight: 1.8,
+                    textAlign: 'center',
+                    margin: '30px'
+                }}>提交失败</div>
+                <div style={{
+                    fontSize: '14px', color: '#00000073',
+                    lineHeight: 1.6,
+                    textAlign: 'center',
+                    width: '70%',
+                    margin: 'auto'
+                }}>
+                    请核对并修改以下信息后，再重新提交。
+                    </div>
             </div>
         )
     }
--- my-react-web/src/components/Success.js
+++ my-react-web/src/components/Success.js
@@ -1,14 +1,30 @@
 import React from 'react'
+import { CheckCircleFilled } from '@ant-design/icons';
 
-export default class Success extends React.Component{
-    constructor(props){
+export default class Success extends React.Component {
+    constructor(props) {
         super(props)
     }
 
-    render(){
+    render() {
         return (
-            <div>
-                Success
+            <div style={{ textAlign: 'center', padding: '24px', backgroundColor: '#fff' }}>
+                <CheckCircleFilled style={{ fontSize: '72px', color: '#52c41a' }} />
+                <div style={{
+                    fontSize: '24px', color: '#000000d9',
+                    lineHeight: 1.8,
+                    textAlign: 'center',
+                    margin: '30px'
+                }}>提交成功</div>
+                <div style={{
+                    fontSize: '14px', color: '#00000073',
+                    lineHeight: 1.6,
+                    textAlign: 'center',
+                    width: '70%',
+                    margin: 'auto'
+                }}>
+                    提交结果页用于反馈一系列操作任务的处理结果， 如果仅是简单操作，使用 Message 全局提示反馈即可。 本文字区域可以展示简单的补充说明，如果有类似展示 “单据”的需求，下面这个灰色区域可以呈现比较复杂的内容。
+                    </div>
             </div>
         )
     }
