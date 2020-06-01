5c65889fbf793d5877fb820016551194e7b1686a
taoli <15230817603@163.com>
2020-06-01 22:29:46 +0800

    setting

--- my-react-web/src/components/Setting.js
+++ my-react-web/src/components/Setting.js
@@ -19,15 +19,24 @@ export default class Setting extends React.Component {
             },
             safeInfo:{
                 label:'安全设置',
-                data:[]
+                data:[
+                    {label:'账户密码',info:'当前密码强度：：强',operator:'修改'},
+                    {label:'密保手机',info:'已绑定手机：：138****8293',operator:'修改'},
+                    {label:'密保问题',info:'未设置密保问题，密保问题可有效保护账户安全',operator:'设置'},
+                    {label:'备用邮箱',info:'已绑定邮箱：：ant***sign.com',operator:'修改'},
+                    {label:'MFA设备',info:'未绑定 MFA 设备，绑定后，可以进行二次确认',operator:'绑定'}
+                ]
             },
             accountInfo:{
-                label:'账号绑定',
-                data:[]
+                label:'账号绑定'
             },
             msgInfo:{
                 label:'新消息通知',
-                data:[]
+                data:[
+                    {label:'账户密码',info:'其他用户的消息将以站内信的形式通知',status:true},
+                    {label:'系统消息',info:'系统消息将以站内信的形式通知',status:true},
+                    {label:'待办任务',info:'待办任务将以站内信的形式通知',status:true}
+                ]
             },
         }
     }
